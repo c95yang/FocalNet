ebbd7c7a7b5fa5817d94654277217be33553e115
chen <c95.yang@tum.de>
2024-03-09 22:25:56 +0100

    changed_save_freq

--- Dehazing/ITS/main.py
+++ Dehazing/ITS/main.py
@@ -67,7 +67,7 @@ if __name__ == '__main__':
     parser.add_argument('--num_epoch', type=int, default=300)#300
     parser.add_argument('--print_freq', type=int, default=100)
     parser.add_argument('--num_worker', type=int, default=8)
-    parser.add_argument('--save_freq', type=int, default=1)
+    parser.add_argument('--save_freq', type=int, default=10)
     parser.add_argument('--valid_freq', type=int, default=1)
     parser.add_argument('--resume', type=str, default='')
     parser.add_argument('--gamma', type=float, default=0.5)
