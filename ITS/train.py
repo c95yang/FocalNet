aef541b665d9ff081b7a6afaf4423e136d4b0b54
chen <c95.yang@tum.de>
2024-03-19 09:43:49 +0100

    wandb debug and use xv6

--- ITS/train.py
+++ ITS/train.py
@@ -30,7 +30,7 @@ def _train(model, args):
         print('Resume from %d'%epoch)
         epoch += 1
 
-    wan = wandb.init(project="dehazing", name='global-local')
+    wan = wandb.init(entity="Low-Level-Vision", project="dehazing", name='global-local')
     wan.config.update(dict(batch_size=args.batch_size, epoch=args.num_epoch))
 
     writer = SummaryWriter()
